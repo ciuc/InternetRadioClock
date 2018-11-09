f3376cce9c710c056dc6e6666685e2fe1458dee7
ciuc <ciuc@keba.com>
2018-11-09 11:17:39 +0200

    Added activities and settings

---
+++ InternetRadioClock/src/main/java/ro/antiprotv/radioclock/NightProfileActivity.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright Cristian "ciuc" Starasciuc 2016
+ * cristi.ciuc@gmail.com
+ */
+package ro.antiprotv.radioclock;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+/**
+ * Created by ciuc on 7/17/16.
+ */
+public class NightProfileActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_night_profile);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        getFragmentManager().beginTransaction()
+                .replace(R.id.settings_content, new SettingsFragment())
+                .commit();
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id== android.R.id.home ){
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
+
+
---
+++ InternetRadioClock/src/main/java/ro/antiprotv/radioclock/NightProfileFragment.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright Cristian "ciuc" Starasciuc 2016
+ * Licensed under the Apache license 2.0
+ * cristi.ciuc@gmail.com
+ */
+package ro.antiprotv.radioclock;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+/**
+ * Created by ciuc on 7/12/16.
+ */
+public class NightProfileFragment extends PreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences_settings);
+
+    }
+}
