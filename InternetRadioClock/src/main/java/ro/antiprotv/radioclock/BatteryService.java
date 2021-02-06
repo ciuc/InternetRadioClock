24fbb37f7fb8985143ca16e9eecb4ff5cd27d1a1
ciuc <cristi.ciuc@gmail.com>
2021-02-06 14:28:52 +0200

    Added battery display Added always wake up on station feature Various code cleanup

---
+++ InternetRadioClock/src/main/java/ro/antiprotv/radioclock/BatteryService.java
@@ -0,0 +1,64 @@
+package ro.antiprotv.radioclock;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.os.BatteryManager;
+import android.preference.PreferenceManager;
+import android.support.v4.content.ContextCompat;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import static android.view.View.GONE;
+import static android.view.View.VISIBLE;
+
+public class BatteryService extends BroadcastReceiver {
+    private final TextView pct;
+    private final SharedPreferences prefs;
+    private final ClockActivity clockActivity;
+    private final ImageView icon;
+
+    public BatteryService(ClockActivity clockActivity, ImageView icon, TextView pct) {
+        this.prefs = PreferenceManager.getDefaultSharedPreferences(clockActivity);
+        this.pct = pct;
+        this.clockActivity = clockActivity;
+        this.icon = icon;
+    }
+
+    public void registerBatteryLevelReceiver() {
+        IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        clockActivity.registerReceiver(this, ifilter);
+        pct.setVisibility(VISIBLE);
+        icon.setVisibility(VISIBLE);
+    }
+
+
+    public void unregisterBatteryLevelReceiver() {
+        try {
+            pct.setVisibility(GONE);
+            icon.setVisibility(GONE);
+            clockActivity.unregisterReceiver(this);
+        } catch (Throwable t) {
+            //nothing
+        }
+    }
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        int status = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+        pct.setText(String.format("%d%%",Integer.valueOf(status)));
+        boolean low = intent.getBooleanExtra(BatteryManager.EXTRA_BATTERY_LOW, false);
+        boolean charging = intent.getIntExtra(BatteryManager.EXTRA_STATUS, -1) == BatteryManager.BATTERY_STATUS_CHARGING;
+        if (low && !charging) {
+            pct.setTextColor(clockActivity.getResources().getColor(R.color.color_clock_red));
+            icon.setColorFilter(ContextCompat.getColor(context, R.color.color_clock_red), android.graphics.PorterDuff.Mode.SRC_IN);
+            icon.setImageResource(R.drawable.ic_baseline_battery_alert_16);
+        } else {
+            pct.setTextColor(clockActivity.getResources().getColor(R.color.color_clock));
+            icon.setImageResource(R.drawable.ic_baseline_battery_std_16);
+            icon.setColorFilter(ContextCompat.getColor(context, R.color.color_clock), android.graphics.PorterDuff.Mode.SRC_IN);
+        }
+    }
+}
